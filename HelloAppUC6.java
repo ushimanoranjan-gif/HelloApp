685a14d8b2800d503a9c3054fb74302273da27fe
ushimanoranjan-gif <ushimanoranjan@gmail.com>
2026-04-21 13:56:39 +0530

    UC6

---
+++ HelloAppUC6.java
@@ -0,0 +1,19 @@
+public class HelloApp {
+    public static void main(String[] args) {
+        if (args.length == 0) {
+            System.out.println("Hello, World!");
+            return;
+        }
+
+        StringBuilder nameBuilder = new StringBuilder();
+
+        for (String name : args) {
+            nameBuilder.append(name).append(", ");
+        }
+
+        if (nameBuilder.length() > 0) {
+            String names = nameBuilder.substring(0, nameBuilder.length() - 2);
+            System.out.println("Hello, " + names + "!");
+        }
+    }
+}
